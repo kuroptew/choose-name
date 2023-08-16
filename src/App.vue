38446ec895def96ad49d3af55bcca8383f7c37fd
kuroptew <kuroptevdb@gmail.com>
2023-08-16 19:34:05 +0300

    Сделал форму для добавления имени

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+import FormName from "./components/FormName.vue";
+export default {
+  components: { FormName },
+};
+</script>
+
+<template>
+  <div :class="$style.container">
+    <h1 :class="$style.title">Выбираем имя ребенку</h1>
+    <div :class="$style.content">
+      <div :class="$style['container_mom']">
+        <form-name sex="female" />
+        <h2 :class="[$style.subtitle, $style['subtitle_mom']]">
+          Имена от мамы
+        </h2>
+      </div>
+      <div :class="$style['container_dad']">
+        <form-name sex="male" />
+        <h2 :class="[$style.subtitle, $style['subtitle_dad']]">
+          Имена от папы
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" module>
+.container {
+  margin: 0 auto;
+  max-width: 1000px;
+}
+.title {
+  margin-bottom: 28px;
+  @include font-size(50, 60);
+  color: $black;
+  text-align: center;
+}
+
+.subtitle {
+  margin-top: 16px;
+  @include font-size(32, 38);
+  text-align: center;
+
+  &_mom {
+    color: $dark-pink;
+  }
+
+  &_dad {
+    color: $dark-blue;
+  }
+}
+
+.content {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/FormName.vue
@@ -0,0 +1,99 @@
+<template>
+  <form @submit.prevent :class="[$style.form, $style[`form_${sex}`]]">
+    <input
+      type="text"
+      :class="$style.input"
+      placeholder="Введите имя"
+      v-model="name"
+    />
+    <input
+      type="text"
+      :class="$style.input"
+      placeholder="Введите рейтинг имени"
+      v-model="rating"
+    />
+    <button
+      @click="addName"
+      :class="[$style.button, $style[`button_${sex}`]]"
+      :disabled="!formValid"
+    >
+      Добавить имя
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    sex: {
+      type: String,
+      default: "male",
+    },
+  },
+  data() {
+    return {
+      name: "",
+      rating: "",
+    };
+  },
+  computed: {
+    formValid() {
+      return this.name.length > 2 && this.rating >= 1;
+    },
+  },
+  methods: {
+    addName() {
+      console.log("name");
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.form {
+  width: 360px;
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  border-radius: 20px;
+  &_female {
+    background-color: $pink;
+  }
+
+  &_male {
+    background-color: $blue;
+  }
+}
+
+.input {
+  padding: 8px;
+  border-radius: 4px;
+  border: none;
+  @include font-size(12, 16);
+  color: $black;
+
+  &::placeholder {
+    color: $beige;
+  }
+}
+
+.button {
+  padding: 12px;
+  border-radius: 8px;
+  color: $black;
+  cursor: pointer;
+
+  &:disabled {
+    opacity: 0.8;
+  }
+
+  &_female {
+    background-color: $dark-pink;
+  }
+
+  &_male {
+    background-color: $dark-blue;
+  }
+}
+</style>
