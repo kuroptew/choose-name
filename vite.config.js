7086fd6f188bfb00be5b3312d7824157d26ed63b
kuroptew <kuroptevdb@gmail.com>
2023-08-19 07:55:14 +0300

    Добавил функционал для развертывания на GitHubPages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/choose-name/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
