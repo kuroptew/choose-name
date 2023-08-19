8aa179c453561e49bc5fc2f5f5e8afd68864a75e
kuroptew <kuroptevdb@gmail.com>
2023-08-19 11:15:35 +0300

    Изменил базовый URL для запросов

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://my-json-server.typicode.com/kuroptew/child-names/";
 
 export async function getNames(namesBy) {
   try {
